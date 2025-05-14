90fcb25e29afe75a4ccd0bb6686048fccc009525
WINDOWS-FRL7E63\WD <bye951@nate.com>
2025-05-14 10:13:41 +0900

    Visual Studio에서 작성한 C#코드
    형 변환하는 법과 언더플로우, 상수, 열거형 상수, var자료형에 대한 내용

---
+++ MainApp.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace _2025_04_22_CSharp
+{
+    //enum DialogResult { YES = 1, NO, CANCEL, CONFIRM, OK};
+    //enum MUSIC { DO=3, RE, ME, FA, SOL, RA, SI}
+    internal class MainApp
+    {
+        static void Main(string[] args)
+        {
+            #region underflow(언더플로우)
+            //float a = 0.9f;
+            //int b = (int)a; //데이터 손실
+
+            //WriteLine($"{a} {b}");
+            //double e = 3.1415d;
+            //int f = (int)e;
+            //WriteLine(f);
+            //int g = -500;
+            //uint h = (uint)g; // 언더플로우
+            //WriteLine(h);
+
+            //int x = -30;
+            //uint y = (uint)x; // 언더플로우
+            //WriteLine(y);
+            #endregion
+
+            #region 형 변환
+            //float hp = 100f;
+            //float damage = 20.5f;
+            //int result = 0;
+
+            //result = (int)(hp -= damage);
+            //WriteLine(result);
+
+            //float c = 1.1f;
+            //int d = (int)c;
+            //WriteLine(d);
+
+            //string a = "12345";
+
+            //int b = int.Parse(a);   
+            //WriteLine(b);
+
+            //string PI = "3.1415";
+            //float fPI = float.Parse(PI); // parsing
+            //WriteLine(fPI);
+
+            //int a = 123;
+            //string b = a.ToString();
+            //WriteLine(b);
+
+            //string e = "123456789";
+            //int f = Convert.ToInt32(e);
+            //WriteLine(f);
+
+            //string g = "1.2345";
+            //float f = Convert.ToSingle(g);
+            //WriteLine(f);
+
+            //string gg = "3.141523456789";
+            //double hh = Convert.ToDouble(gg);
+            //WriteLine(hh);
+            #endregion
+
+            #region 상수
+
+            //상수 변수
+            //int n1 = 0;
+            //WriteLine(++n1);
+            //WriteLine(n1++);
+            //WriteLine(n1);
+
+            //WriteLine(--n1);
+            //WriteLine(n1--);
+            //WriteLine(n1);
+            //const double PI = 3.1415d;
+            //상수 : 선언과 동시에 초기화
+            // PI++; 변수가 아니라서 오류 
+            // const double Max; 선언만
+            //Max = 29.45678d; // 초기화 따로 하면 오류
+
+            //int n1 = 100;
+            //int n2 = (n1++)+ 100;
+            //WriteLine($"{n1} {n2}");     
+            #endregion
+
+            #region 열거형 상수 Nullable, enum
+            //열거형 상수
+            //Write($"{(int)DialogResult.YES}\n");
+            //Write($"{(int)DialogResult.NO}\n");
+            //Write($"{(int)DialogResult.CANCEL}\n");
+            //Write($"{(int)DialogResult.CONFIRM}\n");
+            //Write($"{(int)DialogResult.OK} \n");
+            //Write($"{(int)MUSIC.DO} \n");
+            //Write($"{(int)MUSIC.RE} \n");
+            //Write($"{(int)MUSIC.FA} \n");
+            //Write($"{(int)MUSIC.SOL} \n");
+            //Write($"{(int)MUSIC.RA} \n");
+            //Write($"{(int)MUSIC.SI} \n");
+
+            // 우리가 프로그래밍 하다보면 가끔씩 비어 있는 값을 어떠한 값도 가지지 않는 변수가 필요 할 때 
+            // Nullable을 쓴다. 즉 비어있는 상태
+
+            //int? a = null; // null = 비어있는 값 , ? = null을 허용함
+
+            //WriteLine(a.HasValue); // HasValue == Bool
+
+            //WriteLine(a != null); // 같지않다
+            ////WriteLine(a == null); // 같다
+
+            //a = 3;
+
+            //WriteLine(a.HasValue);
+            //WriteLine(a != null); // 같지않다
+
+            //WriteLine(a!.Value);
+
+            #endregion
+
+            #region var 자료형
+
+            // var는 똑똑한 자료형, C# 형식 검사를 깐깐하게 하지만 예외가 있다 그것이 바로 var
+
+            //var a = 20; var b = "안녕하세요"; // 선언과 동시에 초기화, 지역 변수에서만 선언 가능
+            //var c = 3.1415; var d = "a"; // 선언과 동시에 초기화, 지역변수에서만 선언 가능
+            //WriteLine($"{a} \t {b}\t {c}\t {d}\t");
+
+            // var a = 20;
+            //WriteLine("Type : {0}, Value : {1}", a.GetType(), a);
+            //WriteLine($"Type : {a.GetType()}, Value : {a}");
+
+
+            #endregion
+
+            #region 공용 형식 시스템
+            // 공용 형식 시스템 Common Type System (CTS) 
+
+            //System.Int32 a = 123;
+            //int b = 123;
+
+            //WriteLine($"aType:{a.GetType()} {a}");
+            //WriteLine($"bType:{b.GetType()} {b}");
+            //System.String c = "abc";
+            //WriteLine($"cType :{c.GetType()} {c}");
+            //// string d = "abc";
+
+            #endregion
+
+            #region 문제 1
+
+            //WriteLine("사각형의 너비를 입력하세요.");
+            //string width = ReadLine()!;
+
+            //WriteLine("사각형의 높이를 입력하세요.");
+            //string height = ReadLine()!;
+
+            //WriteLine($"사각형의 너비는 : {width}, 사각형의 높이는 : {height}, 사격형의 넓이는 : {float.Parse(width) * float.Parse(height)}");
+
+            #endregion
+
+            #region 문제5
+            //var a = 2020;
+            //var b = "double";
+
+            //WriteLine($"Type : {a.GetType()}, Value : {a}");
+            //WriteLine($"Type : {b.GetType()}, Value : {b}");
+            #endregion
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using static System.Console;
+namespace _2025_04_22_CSharp
+{
+    internal class Program
+    {
+        //정적 할당 : 미리 할당됨
+        //동적 할당 : 필요할때 마다 할당됨
+        // 매개변수 : 전달 인자를 입력 받는 곳
+        //static void Main(string[] args)
+        //{
+        //    #region Write
+
+        //    //Write("Hi~");
+        //    //Write("\n정수 입력 : \n");
+        //    //int n1 = int.Parse(ReadLine()!);
+        //    //int n2 = int.Parse(ReadLine()!);
+        //    //WriteLine("\n 더하기 곱하기 나누기 나머지 결과 값을 출력 하세요\n");
+
+
+        //    //// WriteLine($" 더한 값 :({n1+n2}) , 곱한 값 ({n1*n2}) 나누기 값 : ({n1/n2}) 나머지 값 : ({n1%n2})");
+        //    //WriteLine($"더하기 : {n1} + {n2} = {n1 + n2}");
+        //    //WriteLine($"곱하기 : {n1} * {n2} = {n1 * n2}");
+        //    //WriteLine($"나누기 : {n1} / {n2} = {n1 / n2}");
+        //    //WriteLine($"나머지 : {n1} % {n2} = {n1 % n2}");
+        //    //// 디버깅 : 버그를 해결하는 곳
+        //    #endregion
+
+        //    #region 메인 메서드 매개변수 args 사용 방법 로직
+        //    //if (args.Length == 0) //문자열의 길이가 0이면
+        //    //{
+        //    //    WriteLine($"2025-04-22-CSharp"); // 라인 출력
+        //    //    return; // 위의 조건에 맞다면 빠져 나감
+        //    //}
+        //    //// 위의 조건이 아니라면 첫번째 문자열을 출력
+        //    //for (int i = 0; i < args.Length; i++) 
+        //    //WriteLine($"2025-04-22-CSharp {args[i]}");
+        //    #endregion
+
+        //    #region 기본 데이터 형식
+        //    //sbyte a = -10;
+        //    //byte b = 40;
+        //    //WriteLine($"a = {a} b = {b} = 뺄셈 : {a-b}");
+
+        //    //short c = -30000;
+        //    //ushort d = 60000;
+        //    //WriteLine($"c = {c} d = {d} = 뺄셈 : {c-d}");
+
+        //    //int e = -1000_0000;
+        //    //uint f = 3_0000_0000; // _ 구분자
+        //    //WriteLine($"e = {e} f = {f} = 뺄셈 : {e - f}");
+
+        //    //long g = -5000_0000_0000;
+        //    //ulong h = 200_0000_0000_0000;
+        //    //WriteLine($"g = {g} h = {h}");
+        //    #endregion
+
+        //    #region 이진수 16진수를 십진수로 변환
+        //    //sbyte a = -10;
+        //    //sbyte b = -5000_0000_0000; // 값이 넘치는 현상은 overFlow라고 한다.
+
+        //    //byte a = 240;// 10진수 리터럴 상수
+        //    //WriteLine($"a = {a}");
+
+        //    ////int sum = 20 + 30;
+        //    //byte b = 0b111_0000; // 2진수 리터럴(literal) 상수
+        //    //WriteLine($"b = {b}");
+
+        //    //byte c = 0xF0; // 16진수 리터럴 상수
+        //    //WriteLine($"c = {c}");
+
+        //    //uint d = 0x1234_abcd; // 16진수 리터럴
+        //    //WriteLine($"d = {d}");
+
+        //    #endregion
+
+        //    #region overFlow 되는 로직
+        //    //byte a = 255;
+        //    //sbyte b = (sbyte)a; // (형변환) 캐스팅
+
+        //    //WriteLine(a);
+        //    //WriteLine(b);
+
+        //    //uint a = uint.MaxValue;
+        //    //a += 1;
+        //    //WriteLine(a);
+
+        //    //uint b = uint.MinValue;
+        //    //WriteLine(b);
+        //    #endregion
+
+        //    #region 부동소수점을 표현하는 자료형들
+
+        //    ////4바이트 소수점 7자리의 정밀도
+        //    //float a = 3.1415_9265_3589_7963f;
+        //    //WriteLine(a);
+        //    ////8바이트 소수점 15~16자리의 정밀도
+        //    //double b = 3.1415_9265_3589_7963_46234d;
+        //    //WriteLine(b);
+        //    ////29~30 자리의 정밀도    16바이트 
+        //    //decimal c = 3.1415_9265_3589_7963_46234123456789m;
+        //    //WriteLine(c);
+        //    #endregion
+
+        //    #region char string의 차이
+
+        //    ////2byte , 글자 하나 또는 정수를 표현 하기도 함 유니코드
+        //    //char a = '안';
+        //    //char b = '녕';
+        //    //char c = '하';
+        //    //char d = '세';
+        //    //char e = '요';
+        //    //string str = "안녕하세요 !";
+
+        //    //WriteLine(a);
+        //    //WriteLine(b);   
+        //    //WriteLine(c);
+        //    //WriteLine(d);   
+        //    //WriteLine(e);
+        //    //WriteLine(str);
+
+        //    #endregion
+
+        //    #region Object
+
+        //    // object 모든 자료형들의 부모, 참조형식 (heap) , 모든 자료형을 받아서 쓸 수가 있다.
+        //    //bool isDead = false; 
+
+        //    //object a = 123;
+        //    //object b = 3.1415;
+        //    //object c = "안녕하세요";
+        //    //// 힙(heap) 참조형 데이터
+        //    //object d = true;
+        //    //WriteLine($"{a}\t {b} \t{c}\t {d}");
+
+        //    //int a = 123;
+        //    //object b = (object)a; // 박싱 : 값 형식을 힙 메모리에 저장하고 참조를 통해 접근할 수 있도록 만드는 것
+        //    //int c = (int)b; // 언박싱 : 박싱된 값은 힙 메모리에 저장된 값 형식이고, 이를 다시 스택 메모리로 옮겨서 원래의 값 형식으로 사용하기 위해 언박싱이 필요함
+
+        //    //WriteLine($"{a} {b} {c}");
+
+        //    #endregion
+            
+        //    //sbyte a = 127; // 0111 1111
+        //    //WriteLine(a);
+
+        //    //int b = (int)a;
+        //    //int x = 128;
+        //    //WriteLine(x);
+        //    //sbyte y = (sbyte)x;
+        //    //WriteLine(y);
+        //}
+    }
+}
